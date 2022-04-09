c9b04f354d8669d15f931e347df79cf033c6a39e
Kelvin Sundli <kelvin.sundli@cognite.com>
2022-04-09 16:43:16 +0200

    define schema

---
+++ src/models/schedule.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+from sqlalchemy import TIMESTAMP, Column, String, func
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()  # type: Any
+
+
+class Schedule(Base):
+    __tablename__ = "schedule"
+    playlist = Column(String())
+    playlist_id = Column(String(), primary_key=True)
+    start_day = Column(String(), primary_key=True)
+    start_time = Column(String(), primary_key=True)
+    last_updated = Column(
+        TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp()
+    )
