043a60761bd686fee71a5e351ff1324866171888
Gubbsjuk <joachimleine@gmail.com>
2022-04-10 00:07:29 +0200

    add config_mdl

--- src/setup_db.py
+++ src/setup_db.py
@@ -2,11 +2,13 @@ import sqlalchemy_utils
 
 from sqlalchemy import create_engine
 
-from models.schedule import Base
+import models.schedule
+import models.config_mdl
 
 
 engine = create_engine("sqlite:///5l.db", echo=True, future=True)
 if sqlalchemy_utils.database_exists(engine.url):
     sqlalchemy_utils.drop_database(engine.url)
 
-Base.metadata.create_all(engine)
+models.schedule.Base.metadata.create_all(engine)
+models.config_mdl.Base.metadata.create_all(engine)
